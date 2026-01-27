044e6f8c4a9ffe3dee75114fd1cd9521536ffe56
Trem Developer <dev@trem.app>
2026-01-27 20:45:28 +0100

    feat: Add tRPC API endpoint using Hono.

---
+++ app/api/[...trpc]+api.ts
@@ -0,0 +1,10 @@
+import { ExpoRequest, ExpoResponse } from 'expo-router/server';
+import app from '../../backend/hono';
+
+export async function GET(req: Request) {
+    return app.fetch(req);
+}
+
+export async function POST(req: Request) {
+    return app.fetch(req);
+}
